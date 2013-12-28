5fa6bc29d9b53a9e5b82a62c4bd679ed8d140d45
Harry Peach <harrypeach1999@gmail.com>
2013-12-28 15:24:39 +0000

    Updated some extra commands

--- connection.py
+++ connection.py
@@ -2,10 +2,10 @@ import socket
 
 HOST="irc.freenode.net"
 PORT=6667
-NICK="Lucy"
+NICK="BOT-Lucy"
 IDENT="FunBot"
 REALNAME="FunBot"
-CHAN="#cephbot"
+CHAN="#fedoracoin"
 readbuffer=""
 
 def send_channel(s, text):
@@ -27,4 +27,4 @@ def get_connection():
 	return s
 
 def announce(s):
-	send_channel(s, "Hello! I am FunBot version 1.0, but you can call me Lucy!")
+	send_channel(s, "Hello! I am FunBot version 1.0, but you can call me Lucy!")#
--- functions.py
+++ functions.py
@@ -10,11 +10,13 @@ def tips(s):
 	connection.send_channel(s, "ELucyYgmNsEa12wrZYnhtvwuRPWcDomxsF.")
 
 def list(s):
-	connection.send_channel(s, "These are the main bot comamnds:")
-	connection.send_channel(s, "\t!leetmeter: See how l33t you are.")
-	connection.send_channel(s, "\t!hello: Say hello to me!")
-	connection.send_channel(s, "\t!credits: Find out who made this bot!")
-	connection.send_channel(s, "\t!tip: Show the address to tip me at!")
+	connection.send_channel(s, "These are the main bot commands:")
+	connection.send_channel(s, "\t-leetmeter: See how l33t you are.")
+	connection.send_channel(s, "\t-hello: Say hello to me!")
+	connection.send_channel(s, "\t-credits: Find out who made this bot!")
+	connection.send_channel(s, "\t-tip: Show the address to tip me at!")
+	connection.send_channel(s, "\t-420blazeit: Smoke some weed with FrenchyLaFleur!")
+	connection.send_channel(s, "\t-source: See me source code! D:")
 
 def greet(s, user):
 	connection.send_channel(s, "Hello, " + user + "!")
@@ -23,4 +25,10 @@ def leet(s):
 	connection.send_channel(s, "You are " + str(randint(1,100)) + "% leet!")
 	
 def myst(s):
+	connection.send_channel(s, "Here is my source: https://github.com/Cephon/FunBot")
+	
+def git(s):
 	connection.send_channel(s, "Sawg - MystPhysX 2013")
+	
+def blaze(s, user):
+	connection.send_channel(s, user + " smokes 81 blunt ones with FrenchyLaFleur.")#
