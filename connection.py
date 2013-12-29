7ef6726809cb5c90dafc2c3a266087b957c3e891
Harry Peach <harrypeach1999@gmail.com>
2013-12-29 14:55:40 +0000

    Revert "Changed command prefixes and added -leave"
    This reverts commit 59b8184f0e49363448493c2098e9733c538a3cc8.

--- connection.py
+++ connection.py
@@ -2,10 +2,10 @@ import socket
 
 HOST="irc.freenode.net"
 PORT=6667
-NICK="BOT-Lucy"
+NICK="Lucy"
 IDENT="FunBot"
 REALNAME="FunBot"
-CHAN="#fedoracoin"
+CHAN="#cephbot"
 readbuffer=""
 
 def send_channel(s, text):
@@ -27,4 +27,4 @@ def get_connection():
 	return s
 
 def announce(s):
-	send_channel(s, "Hello! I am FunBot version 1.0, but you can call me Lucy!")#
+	send_channel(s, "Hello! I am FunBot version 1.0, but you can call me Lucy!")
--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 import requests
 import connection
 from random import randint
-import time
 	
 def credits(s):
 	connection.send_channel(s, "My creator is Cephon!")
@@ -11,13 +10,11 @@ def tips(s):
 	connection.send_channel(s, "ELucyYgmNsEa12wrZYnhtvwuRPWcDomxsF.")
 
 def list(s):
-	connection.send_channel(s, "These are the main bot commands:")
-	connection.send_channel(s, "\t-leetmeter: See how l33t you are.")
-	connection.send_channel(s, "\t-hello: Say hello to me!")
-	connection.send_channel(s, "\t-credits: Find out who made this bot!")
-	connection.send_channel(s, "\t-tip: Show the address to tip me at!")
-	connection.send_channel(s, "\t-420blazeit: Smoke some weed with FrenchyLaFleur!")
-	connection.send_channel(s, "\t-source: See me source code! D:")
+	connection.send_channel(s, "These are the main bot comamnds:")
+	connection.send_channel(s, "\t!leetmeter: See how l33t you are.")
+	connection.send_channel(s, "\t!hello: Say hello to me!")
+	connection.send_channel(s, "\t!credits: Find out who made this bot!")
+	connection.send_channel(s, "\t!tip: Show the address to tip me at!")
 
 def greet(s, user):
 	connection.send_channel(s, "Hello, " + user + "!")
@@ -26,21 +23,4 @@ def leet(s):
 	connection.send_channel(s, "You are " + str(randint(1,100)) + "% leet!")
 	
 def myst(s):
-	connection.send_channel(s, "MONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEYMONEY")
-	
-def hack(s):
-    connection.send_channel(s, "Opening bridge port to http://fedoraco.in/")
-    time.sleep(1)
-    connection.send_channel(s, "Attempting XSS Method 1")
-    time.sleep(2)
-    connection.send_channel(s, "Fail")
-    time.sleep(1)
-    connection.send_channel(s, "Attempting XSS Method 2")
-    time.sleep(1)
-    connection.send_channel(s, "You're a faggot")
-	
-def git(s):
-	connection.send_channel(s, "Here is my source: https://github.com/Cephon/FunBot")
-	
-def blaze(s, user):
-	connection.send_channel(s, user + " smokes 81 blunt ones with FrenchyLaFleur.")
+	connection.send_channel(s, "Sawg - MystPhysX 2013")
